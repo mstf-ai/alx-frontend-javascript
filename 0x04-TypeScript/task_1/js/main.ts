7af9a06a2de9a0a00f908524689da9408c6ef084
mstf-ai <albrnsmstfy252@gmail.com>
2025-09-10 15:50:00 +0300

    M

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,3 +30,13 @@ const director1: Director = {
 };
 
 console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe")); // J. Doe
