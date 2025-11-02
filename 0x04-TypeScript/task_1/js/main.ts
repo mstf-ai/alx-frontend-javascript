a2535fb494fbb6c65ac29aaacca8fadcb680353e
mstf-ai <albrnsmstfy252@gmail.com>
2025-11-02 14:14:50 +0200

    M

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -23,8 +23,8 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
+function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
-};
+}
 
 console.log(printTeacher("John", "Doe"));
