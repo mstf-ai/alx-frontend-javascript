57de958b0872115835bb3b50841d2d40507accde
mstf-ai <albrnsmstfy252@gmail.com>
2025-11-02 22:39:02 +0200

    M

--- 0x04-TypeScript/task_3/js/interface.ts
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -1,7 +1,5 @@
-// Create a type RowID and set it equal to number
 export type RowID = number;
 
-// Create an interface RowElement
 export interface RowElement {
   firstName: string;
   lastName: string;
